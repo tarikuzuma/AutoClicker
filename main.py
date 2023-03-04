5ccab509581d206e99c215799d9c773cba5386c5
Tarikuzuma <83963273+tarikuzuma@users.noreply.github.com>
2023-03-04 23:24:43 +0800

    Looped through main
    Made an array that is appended every time a new coordinate is put, also made a pseudo loop that is subjected to change

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import pyautogui
-import random
+import pyautogui as pag
 import time
+
+coords = []
 
 while True:
     x, y = pyautogui.position()
     positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
     print(positionStr, sep ='\n')
+    
+    coords.append((x,y))
+    #coords.append((positionStr))
+    print ("")
+    print (*coords)
     time.sleep(2)
+
+
+    #Make a loop that loops through coords when done
+    #Loop subjected to change holy fuck
+    
+    for i in range(len(coords)):
+        pag.moveTo(coords[i])
+        print(coords[i])
+    #another big problem is to convert the coordiante into one list, hence 1 string since append only accepts one argument.
     
