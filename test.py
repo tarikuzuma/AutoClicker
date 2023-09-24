cbb1bcec810aee64de339d83f2794813f2e34d6d
Tarikuzuma <83963273+tarikuzuma@users.noreply.github.com>
2023-09-24 19:53:43 +0800

    Add files via upload
    Added option to add time

--- test.py
+++ test.py
@@ -1,31 +1,51 @@
 from pynput.mouse import Listener
 from pynput.keyboard import Listener as KeyboardListener, Key
+import pyautogui as pag
+import time
 
 coords = []
 terminate = False
+seconds = []
+
+def click(coords):
+    for i in range (len(coords)):
+        pag.click(coords[i])
+        time.sleep(seconds[i])
+        print ("Moved to: ", coords[i], "for: ", seconds[i], "seconds")
+
+
+def timeCheck(coords):
+    global seconds
+    for i in range(len(coords)):
+        time_input = input(f"Enter time for coordinate {i + 1}: ")
+        try:
+            seconds.append(float(time_input))
+        except ValueError:
+            print("Invalid input. Please enter a numeric value for time.")
+
+    print("Times entered: ", seconds)
+    click(coords)
 
 def onClick(x, y, button, pressed): #onClick accepts parameters such as x, y, button, pressed
     global terminate
     if pressed and not terminate:
         print('Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
         coords.append((x,y))
-        print (terminate)
 
 def onKeyboardPress(key): #Accepts parameter key which is escape.
     global terminate
     if key == Key.esc:
         print('Exiting...')
         terminate = True
-        return False
+        print (coords)
+        timeCheck(coords)
+        quit()
 
 # Create a keyboard listener
 keyboard_listener = KeyboardListener(on_press=onKeyboardPress)
 
 # Start the keyboard listener in a separate thread
 keyboard_listener.start()
-
     
 with Listener(on_click=onClick) as listener:
     listener.join()
-
-
